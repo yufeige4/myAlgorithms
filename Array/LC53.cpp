1670d9b8d0c2d709887bd7c7f46c381912127b68
yufeige4 <gyfuiuc@gmail.com>
2022-04-07 20:11:30 +0800

    LC350 LC121

--- Array/LC53.cpp
+++ Array/LC53.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+// 53. 最大子数组和
+
+// 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+// 子数组 是数组中的一个连续部分。
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
