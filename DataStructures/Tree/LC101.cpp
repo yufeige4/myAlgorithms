12c34edc257c978213b731d5b013cb512e8c9a2f
yufeige4 <gyfuiuc@gmail.com>
2022-04-10 18:27:55 +0800

    LC 101

--- DataStructures/Tree/LC101.cpp
+++ DataStructures/Tree/LC101.cpp
@@ -2,3 +2,37 @@
 
 using namespace std;
 
+// 101. 对称二叉树
+
+// 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isSymHelper(TreeNode* left,TreeNode* right){
+        if(!left&&!right){
+            return true;
+        }
+        if(!left||!right){
+            return false;
+        }
+        if(left->val!=right->val){
+            return false;
+        }
+        return isSymHelper(left->left,right->right) && isSymHelper(left->right,right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root){
+            return false;
+        }
+        return isSymHelper(root->left,root->right);
+    }
+};
